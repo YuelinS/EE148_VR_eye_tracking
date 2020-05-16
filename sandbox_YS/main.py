a300dd831ea03ebd778076ec740855a57777ff72
Yuelin_Shi <ysshi@caltech.com>
2020-05-16 14:46:46 -0700

    finished version

--- sandbox_YS/main.py
+++ sandbox_YS/main.py
@@ -14,7 +14,7 @@ import pickle
 from EyeTracking import EyeTrackingDataset
 
 '''
-python main.py --batch-size 64 --epochs 1 --log-interval 10 --lr 1
+python main.py --batch-size 128 --epochs 1 --log-interval 20 --lr 0.1
 python main.py --evaluate --load-model eye_tracking_model.pt
 '''
 
@@ -25,46 +25,49 @@ class Net(nn.Module):
         super(Net, self).__init__()        
         self.args = args
         
-        out1, out2, out3, out_last = 16, 32, 64, 64 
-        self.ker1, self.ker2, self.ker3, self.ker4 = 2, 4, 4, 4
-        self.stride1, self.stride2, self.stride3, self.stride4 = 1, 1, 1, 2
+        chns = [16, 32, 64, 64]
+        self.kers = [3, 3, 3, 3]
+        self.strides = [1, 1, 1, 1]
 
         lin_in_w = self.calculate_size(w_rs)
         lin_in_h = self.calculate_size(h_rs)
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=out1, kernel_size=self.ker1, stride = self.stride1)
-        self.conv2 = nn.Conv2d(out1, out2, self.ker2, self.stride2)
-        self.conv3 = nn.Conv2d(out2, out3, self.ker3, self.stride3)
-        self.conv4 = nn.Conv2d(out3, out_last, self.ker4, self.stride4)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=chns[0], kernel_size=self.kers[0], stride = self.strides[0])
+        self.conv2 = nn.Conv2d(chns[0], chns[1], self.kers[1], self.strides[1])
+        self.conv3 = nn.Conv2d(chns[1], chns[2], self.kers[2], self.strides[2])
+        self.conv4 = nn.Conv2d(chns[2], chns[3], self.kers[3], self.strides[3])
 
-        self.fc1 = nn.Linear(out_last*lin_in_h*lin_in_w, 64)
-        self.fc2 = nn.Linear(64, 16)       
+        self.fc1 = nn.Linear(chns[-1]*lin_in_h*lin_in_w, 16)
+        self.fc2 = nn.Linear(16, 16)       
         self.fc3 = nn.Linear(16, 3)
         
+        self.activate = nn.ELU()
+        
         self.forward_pass = nn.Sequential(
-            self.conv1, nn.ReLU(), nn.Dropout2d(0.8), nn.BatchNorm2d(out1),
-            self.conv2, nn.ReLU(), nn.MaxPool2d(2, stride=2), nn.Dropout2d(0.8), nn.BatchNorm2d(out2),
-            self.conv3, nn.ReLU(), nn.MaxPool2d(2, stride=2), nn.Dropout2d(0.8), nn.BatchNorm2d(out3),
-            self.conv4, nn.ReLU(), nn.MaxPool2d(2, stride=2), nn.Dropout2d(0.8), nn.BatchNorm2d(out_last),
-            nn.Flatten(), self.fc1, nn.ReLU(),
-            self.fc2, nn.ReLU(),
+            self.conv1, nn.BatchNorm2d(chns[0]), self.activate, nn.MaxPool2d(2), nn.Dropout2d(0.8), 
+            self.conv2, nn.BatchNorm2d(chns[1]), self.activate, nn.MaxPool2d(2), nn.Dropout2d(0.8), 
+            self.conv3, nn.BatchNorm2d(chns[2]), self.activate, nn.MaxPool2d(2), nn.Dropout2d(0.8), 
+            self.conv4, nn.BatchNorm2d(chns[3]), self.activate, nn.MaxPool2d(2), nn.Dropout2d(0.8), 
+            nn.Flatten(), self.fc1, self.activate,
+            self.fc2, self.activate,
             self.fc3
         )
         
-        self.criterion = nn.MSELoss()
+        self.criterion_train = nn.MSELoss()
+        self.criterion_test = nn.MSELoss(reduction='sum')
         # Try different optimzers here [Adadelta, Adam, SGD, RMSprop]
         self.optimizer = optim.Adam(self.parameters(), lr=args.lr)
-        self.scheduler = ReduceLROnPlateau(self.optimizer, 'min')
-        # self.scheduler = StepLR(self.optimizer, step_size=args.step, gamma=args.gamma)
+        # self.scheduler = ReduceLROnPlateau(self.optimizer, 'min')
+        self.scheduler = StepLR(self.optimizer, step_size=args.step, gamma=args.gamma)
     
     
     def calculate_size(self, size_in):
         # conv_out = (conv_in + 2×padding - kernel_size) / stride +1
 
-        size_out = size_in - self.ker1 + 1
-        size_out = np.floor((size_out - self.ker2 + 1) / 2)
-        size_out = np.floor((size_out - self.ker3 + 1) / 2)
-        size_out = np.floor(np.floor((size_out - self.ker4) / self.stride4 +1) / 2)
+        size_out = np.floor((size_in - self.kers[0] + 1) / 2)
+        size_out = np.floor((size_out - self.kers[1] + 1) / 2)
+        size_out = np.floor((size_out - self.kers[2] + 1) / 2)
+        size_out = np.floor(np.floor((size_out - self.kers[3]) / self.strides[3] +1) / 2)
 
         return int(size_out)
     
@@ -77,13 +80,19 @@ class Net(nn.Module):
         #     print(x.size())
         # output = x
         
-        loss = self.criterion(output, target)
-        
+            
         if self.training:  
+            loss = self.criterion_train(output, target)     
+
+            for param in self.parameters():    # Compute regularization
+                loss += self.args.reg_lambda*torch.mean(torch.abs(param))
+                
             self.optimizer.zero_grad()               # Clear the gradient              
             loss.backward()                     # Gradient computation
             self.optimizer.step() 
-            
+        else:
+             loss = self.criterion_test(output, target)     
+
         return output,loss
 
         
@@ -94,38 +103,48 @@ class Net(nn.Module):
             output, loss = self.forward(data, target)                # Make predictions
 
             if batch_idx % self.args.log_interval == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.4f}'.format(
                     epoch, batch_idx * len(data), len(train_loader.dataset),
                     100. * batch_idx / len(train_loader), loss.item()))
-                            
+                                
                 print('Examples:')
                 for i in range(2):
                     print('True:', [f'{target.tolist()[i][d]:.3f}' for d in range(3)], 
                           'Pred:', [f'{output.tolist()[i][d]:.3f}' for d in range(3)])
                 
                 total_time = time.time() - start_time
-                print(f'Time per {self.args.log_interval} iter: {total_time}s\n')
+                print(f'Time per {self.args.log_interval} iters: {total_time:.2f}s\n')
                 
         return loss
            
     def test_iterate(self, device, test_loader):     
         
         test_loss = 0   
+        preds = []
+        trues = [] 
         
         for batch_idx, (data, target) in enumerate(test_loader):
             data, target = data.to(device,dtype=torch.float), target.to(device,dtype=torch.float)           
             output, loss = self.forward(data, target)                # Make predictions
-            test_loss += loss  # sum up batch loss        
-
+            test_loss += loss  # sum up batch loss                        
+        
+            if self.args.evaluate:
+              print(f'Test batch {batch_idx}')              
+              trues.append(target.detach().numpy())
+              preds.append(output.detach().numpy())  
+              
+            # if batch_idx == 0:
+            #       break
+                
         test_loss /= len(test_loader.dataset)
     
-        print('\nTest set: Average loss: {:.4f} \n'.format(test_loss))
+        print('\nTest set: Average loss: {:.4f}'.format(test_loss))
         print('Examples:')
         for i in range(3):
             print('True:', [f'{target.tolist()[i][d]:.3f}' for d in range(3)], 
                   'Pred:', [f'{output.tolist()[i][d]:.3f}' for d in range(3)])
                 
-        return test_loss
+        return test_loss, trues, preds
         
         
         
@@ -136,9 +155,9 @@ def main():
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('--batch-size', type=int, default=200, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+    parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=2, metavar='N',
+    parser.add_argument('--epochs', type=int, default=1, metavar='N',
                         help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
                         help='learning rate (default: 1.0)')
@@ -160,7 +179,8 @@ def main():
                         help='For Saving the current Model')
     parser.add_argument('--data-partition', type=int, default=1, metavar='N',
                         help='Choose subset of  training set (default: 1)')
-    
+    parser.add_argument('--reg-lambda', type=float, default=0.0008, metavar='L',
+                        help='Regularization lambda (default:0.0008)')    
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
@@ -170,7 +190,7 @@ def main():
     
     partition = args.data_partition
     
-    h_rs, w_rs = 96,256
+    h_rs, w_rs = 60,160
     path_pos = '../../data/Fixation Training Pos.bin'
     dir_images = '../../data/Fixation Training Images'
     
@@ -180,11 +200,10 @@ def main():
     
     # version = '_augmented'
     img_transform = transforms.Compose([
-        # transforms.RandomRotation(10),
-        # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0),
         transforms.Resize((h_rs,w_rs)),
+        transforms.Grayscale(),
+        transforms.ColorJitter(brightness=0.05, contrast=0.05),
         transforms.ToTensor()])
-        # transforms.Normalize((0.1307,),(0.3081,))
         
     full_dataset = EyeTrackingDataset(path_pos = path_pos, dir_images = dir_images, transform=img_transform)
        
@@ -193,6 +212,7 @@ def main():
     val_length = int(0.1 * len(full_dataset))
     test_length = len(full_dataset) - train_length - val_length
 
+    np.random.seed(1)
     train_dataset, val_dataset, test_dataset=torch.utils.data.random_split(full_dataset,(train_length,val_length,test_length))
 
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=0)
@@ -209,15 +229,20 @@ def main():
         model.load_state_dict(torch.load(args.load_model))
         
         model.eval()
-        
-        test_loss = model.test_iterate(device,test_loader)
-        np.save(rfd + 'loss_test.npy', test_loss)
+        with torch.no_grad():
+            test_loss, trues, preds = model.test_iterate(device,test_loader)
+            np.save(rfd + 'loss_test.npy', test_loss.detach().numpy())
+            np.save(rfd + 'model_prediction.npy',[trues.detach().numpy(),preds.detach().numpy()])
 
         return    
     
  
 #%% Train model
-
+    if os.path.exists(rfd + 'best_loss.npy'):
+        best_loss = np.load(rfd + 'best_loss.npy')
+    else:   
+        best_loss = 15
+    
     model = Net(h_rs,w_rs,args).to(device)
     
     train_batch_losses, val_losses = [],[]
@@ -229,15 +254,26 @@ def main():
         
         model.eval()                       
         with torch.no_grad():
-            val_loss = model.test_iterate(device,val_loader)  
-            
+            val_loss, _ , _ = model.test_iterate(device,val_loader)
+                    
+        # remember best loss and save   
+        val_loss = val_loss.detach().numpy()             
+        if args.save_model and val_loss < best_loss:            
+            best_loss = val_loss           
+            torch.save(model.state_dict(), 'eye_tracking_model.pt')
+            np.save(rfd + 'best_loss.npy', best_loss)
+            best_loss = val_loss
+        
+        # record train & val loss for every epoch 
         train_batch_losses.append(train_batch_loss)
         val_losses.append(val_loss)
+            
+    np.save(rfd + 'loss_train.npy', [train_batch_losses.detach().numpy(),val_losses])
+            
 
-
-    if args.save_model:        
-        torch.save(model.state_dict(), 'eye_tracking_model.pt')
-        np.save(rfd + 'loss_train.npy', [train_batch_losses,val_losses])
+    # if args.save_model:        
+    #     torch.save(model.state_dict(), 'eye_tracking_model.pt')
+    #     np.save(rfd + 'loss_train.npy', [train_batch_losses,val_losses])
         
         # For ploting partition - train:
         # torch.save(model.state_dict(), 'mnist_model' + str(model_sel) + version  + '_part.pt')
@@ -247,31 +283,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-#%% 2. Kernels visualization
-# rfd = 'D:/git/results/'    
-# import matplotlib.pyplot as plt
-
-
-# model = Net()
-# model.load_state_dict(torch.load('./mnist_model2.pt'))
-# c1w = model.state_dict()['conv1.weight'].numpy()
-
-# fig,axs = plt.subplots(3,3,figsize=(15,15))
-# axs = axs.ravel()
-# for i in range(8):  
-#     axs[i].imshow(np.squeeze(c1w[i,0]),cmap = 'gray')
-#     # axs[i].set_title(f'True: {trues[i]}, Pred: {preds[i][0]}')
-    
-# plt.savefig(rfd + 'kernels.png')
-
-
-
-
-
-
-
-
 
---
+++ sandbox_YS/visualize.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 22:37:07 2020
+
+@author: shiyl
+"""
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+rfd = 'D:/git/EE148/results_project/'    
+
+
+#%% Training loss curve
+from matplotlib.ticker import MaxNLocator
+
+[train_batch_losses,val_losses] = np.load(rfd+'loss_train.npy',allow_pickle=True)
+test_loss = np.load(rfd+'loss_test.npy')
+
+fig, ax = plt.subplots(1, 1, figsize=(20, 10))  
+ax.plot(train_batch_losses,marker=".")
+ax.plot(val_losses,marker=".")
+ax.grid()
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax.set_xlabel('# Epoch')
+ax.set_ylabel('Loss')
+ax.set_title(f'Test Loss: {test_loss:.3f}')
+
+plt.savefig(rfd + 'training_loss_curve.png')
+
+
+#%% Learning curve
+
+file_names = sorted(os.listdir(rfd)) 
+
+train_accs, test_accs = [], []
+parts = [1,2,4,8,16][::-1] 
+
+for partition in parts:
+    
+    train_fn = [f for f in file_names if 'part'+str(partition) in f and 'train' in f] 
+    test_fn = [f for f in file_names if 'part'+str(partition) in f and 'test' in f] 
+    
+    train_acc = np.load(rfd + train_fn[0])[2,-1]
+    test_acc =  np.load(rfd + test_fn[0])
+    
+    train_accs.append(train_acc)
+    test_accs.append(test_acc)
+
+
+n_train = 60000*.15
+n_parts = [n_train/part for part in parts]
+
+
+plt.figure(figsize=(20, 10))  
+plt.loglog(n_parts,train_accs,marker=".")
+plt.loglog(n_parts,test_accs,marker=".")
+plt.grid()
+# plt.xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.xticks(label=[str(part) for part in parts])
+plt.xlabel('Training data size')
+plt.ylabel('Acccuracy')
+plt.legend({'Train','Test'})
+# plt.set_title('Loss')
+
+
+plt.savefig(rfd + 'data_partition.png')
+
+
+
+#%% visualize kernels
+
+import torch
+
+params = torch.load('./eye_tracking_model2.pt',map_location=torch.device('cpu'))
+c1w = params['conv1.weight'].numpy()
+
+
+fig,axs = plt.subplots(3,3,figsize=(15,15))
+axs = axs.ravel()
+for i in range(8):  
+    axs[i].imshow(np.squeeze(c1w[i,0]),cmap = 'gray')
+    # axs[i].set_title(f'True: {trues[i]}, Pred: {preds[i][0]}')
+    
+plt.savefig(rfd + 'kernels.png')
+
+
+
+#%% Inspect predictions
+
+from mpl_toolkits.mplot3d import Axes3D
+
+files = np.load(rfd + 'model_prediction.npz')
+
+trues = files['arr_0']
+preds = np.squeeze(files['arr_1'])
+batch_size = np.shape(trues)[1]
+
+ibatch = 0
+pairs = np.dstack((trues[ibatch,:,:], preds[ibatch,:,:]))
+
+
+fig = plt.figure(figsize=(15, 10), tight_layout=True)
+ax = fig.add_subplot(111, projection='3d')
+
+for k in range(batch_size):
+    ax.plot(pairs[k, 0, :], pairs[k, 1, :], pairs[k, 2, :])
+    
+ax.invert_yaxis()
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.view_init(45, 60)
+
+plt.savefig(rfd + 'model_prediction.png')
+
+
+
+#%% Incorrect examples
+
+[imgs,trues,preds] = np.load(rfd + 'incorrect_examples.npy')
+
+fig,axs = plt.subplots(3,3,figsize=(15,15))
+axs = axs.ravel()
+for i in range(9):  
+    axs[i].imshow(np.squeeze(imgs[i]),cmap = 'gray')
+    axs[i].set_title(f'True: {trues[i]}, Pred: {preds[i][0]}')
+    
+plt.savefig(rfd + 'incorrect_examples.png')
+
+
+
+#%% feature
+from sklearn.manifold import TSNE
+
+files = np.load(rfd + 'feature_data.npz')
+
+features = files['arr_0']
+labels = files['arr_1'].ravel()
+
+## t-SNE
+feat = features.reshape(-1,64)
+X_embedded = TSNE(n_components=2).fit_transform(feat)
+
+color = np.array([[230, 25, 75], [60, 180, 75], [255, 225, 25],[0, 130, 200], [245, 130, 48], [230, 190, 255],
+                  [170, 110, 40], [255, 250, 200], [210, 245, 60], [250, 190, 190]])
+
+fig, ax = plt.subplots()
+for digit in range(10):
+
+    x = X_embedded[labels==digit,0]
+    y = X_embedded[labels==digit,1] 
+    ax.scatter(x, y, alpha=0.8, c=tuple(color[digit]/255), edgecolors='none', s=5,label=digit) #,cmap = 'winter') #, 
+
+plt.legend()
+plt.savefig(rfd + 't-SNE.png')
+
+
+#%% 8neighbors
+from scipy.spatial.distance import euclidean as euclidian
+import pickle
+
+ims_pos = []
+
+for k in range(5):
+    
+    x0 = feat[k]
+    ds = [euclidian(x0, xi) for xi in feat]
+    d9 = sorted(ds)[:9]
+    p9 = [ds.index(d) for d in d9]
+    
+    im_pos = [[p//1000,p%1000] for p in p9]
+    ims_pos.append(im_pos)
+
+# np.save('8neighbors_pos',ims_pos)
+with open(rfd+"8neighbors_pos.txt", "wb") as fp:   #Pickling
+   pickle.dump(ims_pos, fp)
+
+
+
+
+## images
+imgs = np.load(rfd+'8neighbors_img.npy')
+
+fig,axs = plt.subplots(5,9,figsize=(25,15))
+
+for i in range(5): 
+    for j in range(9):
+        
+        axs[i,j].imshow(np.squeeze(imgs[i,j]),cmap = 'gray')
+    
+plt.savefig(rfd + '8neighbors_grid.png')
+
+
